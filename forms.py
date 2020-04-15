8dd71aa5bc7335282aa4cf4cfa154cefc83e9b8a
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-15 17:35:40 +0100

    modified forms.py

--- forms.py
+++ forms.py
@@ -22,32 +22,32 @@ class LoginForm(FlaskForm):
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
 	
-posts = [
-	{
-		'author': 'Corey Schafer',
-		'title': 'Blog Post 1',
-		'content': 'Blog Post',
-		'date_posted': 'April 20, 2018'
-	},
-	{
-		'author': 'Babatunde Koiki',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'April 21, 2018'
-	},
-	{
-		'author': 'Sodiq Agunbiade',
-		'title': 'Blog Post 3',
-		'content': 'Third post content',
-		'date_posted': 'April 22, 2018'
-	},
-	{
-		'author': 'Sodiq Akinjobi',
-		'title': 'Blog Post 4',
-		'content': 'Fourth post content',
-		'date_posted': 'April 23, 2018'
-	}
-]
+# posts = [
+# 	{
+# 		'author': 'Corey Schafer',
+# 		'title': 'Blog Post 1',
+# 		'content': 'Blog Post',
+# 		'date_posted': 'April 20, 2018'
+# 	},
+# 	{
+# 		'author': 'Babatunde Koiki',
+# 		'title': 'Blog Post 2',
+# 		'content': 'Second post content',
+# 		'date_posted': 'April 21, 2018'
+# 	},
+# 	{
+# 		'author': 'Sodiq Agunbiade',
+# 		'title': 'Blog Post 3',
+# 		'content': 'Third post content',
+# 		'date_posted': 'April 22, 2018'
+# 	},
+# 	{
+# 		'author': 'Sodiq Akinjobi',
+# 		'title': 'Blog Post 4',
+# 		'content': 'Fourth post content',
+# 		'date_posted': 'April 23, 2018'
+# 	}
+# ]
 def load_db():
 	with open('database.json') as f:
 		return json.load(f)
